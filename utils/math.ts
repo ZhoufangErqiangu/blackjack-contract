061a8ac905078930b1a19815e34fee744d8ea9f1
alex <liuhlightning@qq.com>
2024-08-08 11:36:10 +0800

    feat math

--- utils/math.ts
+++ utils/math.ts
@@ -5,14 +5,16 @@
  * @returns bigint
  */
 export function n2b(n: number, decimals: number | bigint = 9): bigint {
+  const dn = Number(decimals);
+  // do not use toFixed here, toFixed has Floating-point traps issue
   const ns = n.toString();
   let [int, dec] = ns.split(".");
   if (int === "0") int = "";
   if (!dec) dec = "";
-  if (dec.length <= Number(decimals)) {
-    dec = dec.padEnd(Number(decimals), "0");
+  if (dec.length <= dn) {
+    dec = dec.padEnd(dn, "0");
   } else {
-    dec = dec.slice(0, Number(decimals));
+    dec = dec.slice(0, dn);
   }
   return BigInt(`${int}${dec}`);
 }
@@ -24,12 +26,13 @@ export function n2b(n: number, decimals: number | bigint = 9): bigint {
  * @returns number
  */
 export function b2n(b: bigint, decimals: number | bigint = 9): number {
-  const bs = b.toString();
-  if (bs.length <= Number(decimals)) {
-    return parseFloat(`0.${bs.padStart(Number(decimals), "0")}`);
+  const dn = Number(decimals);
+  const bs = b.toString(10);
+  if (bs.length <= dn) {
+    return parseFloat(`0.${bs.padStart(dn, "0")}`);
   } else {
     return parseFloat(
-      `${bs.slice(0, bs.length - Number(decimals))}.${bs.slice(bs.length - Number(decimals))}`,
+      `${bs.slice(0, bs.length - dn)}.${bs.slice(bs.length - dn)}`,
     );
   }
 }
